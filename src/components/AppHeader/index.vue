6b0d67a62c970da1824a2e9f9016ce7cefb41840
bwmelon <1454490647@qq.com>
2019-10-19 12:21:29 +0800

    :sparkles: 完成后台退出功能

--- src/components/AppHeader/index.vue
+++ src/components/AppHeader/index.vue
@@ -6,25 +6,53 @@
         <i class="el-icon-arrow-down el-icon--right"></i>
       </span>
       <el-dropdown-menu slot="dropdown">
-        <el-dropdown-item command="a">修改密码</el-dropdown-item>
-        <el-dropdown-item command="b">退出登录</el-dropdown-item>
+        <el-dropdown-item command="a" icon="el-icon-edit">修改密码</el-dropdown-item>
+        <el-dropdown-item command="b" icon="el-icon-s-fold">退出登录</el-dropdown-item>
       </el-dropdown-menu>
     </el-dropdown>
   </div>
 </template>
 
 <script>
+import {logout} from "@/api/login";
 export default {
   methods: {
     handleCommand(command) {
-      this.$message("click on item " + command);
+      switch (command) {
+        case "a":
+          this.$message("修改密码");
+          break;
+
+        case "b":
+          const token = localStorage.getItem("faka-token")
+          logout(token).then(res => {
+            const resp = res.data;
+            if (resp.flag) {
+              this.$message({
+                message: resp.message,
+                type: "success"
+              });
+              localStorage.removeItem("faka-token")
+              localStorage.removeItem("faka-user")
+              this.$router.push('admin')
+            } else {
+              this.$message({
+                message: resp.message,
+                type: "warning"
+              });
+            }
+          });
+          break;
+
+        default:
+          break;
+      }
     }
   }
 };
 </script>
 
 <style scoped>
-
 .el-dropdown {
   float: right;
   margin-right: 20px;
